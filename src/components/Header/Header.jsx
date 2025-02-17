e6cfa7e049c139df97d1d0371385ac4915776cff
Bayastan <bsbaha195@mail.com>
2025-02-17 19:53:17 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaBars } from "react-icons/fa";  
+import "./Header.css";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false); 
   return (
-    <div>
-      Header
+    <div className="header">
+    <div className="header-top">
+      <Link> <img src="https://www.themealdb.com/images/logo-small.png" alt="Logo" /></Link>
+      <button className="burger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <FaBars />
+      </button>
+      <div className={`header-btn ${menuOpen ? "open" : ""}`}>
+        <Link><button>Home</button></Link>
+        <Link><button>API</button></Link>
+        <input type="text" placeholder="Search" />
+      </div>
     </div>
+  </div>
   )
 }
 
